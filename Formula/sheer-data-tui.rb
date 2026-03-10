11c5ab6e6d4b1031165f87e8e98f05d4f53e9467
tom-tilley <tom@sheerdata.com>
2026-03-10 16:05:11 -0400

    chore: update sheer-data-tui to 0.1.0

--- Formula/sheer-data-tui.rb
+++ Formula/sheer-data-tui.rb
@@ -7,20 +7,20 @@ class SheerDataTui < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/SheerData/sheer-data-tui/releases/download/v#{version}/sheer-data-tui-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/SheerData/sheer-data-tui/releases/download/v#{version}/sheer-data-tui-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/SheerData/sheer-data-tui/releases/download/v#{version}/sheer-data-tui-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/SheerData/sheer-data-tui/releases/download/v#{version}/sheer-data-tui-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
@@ -29,6 +29,6 @@ class SheerDataTui < Formula
   end
 
   test do
-    assert_match "sheer-data-tui", shell_output("#{bin}/sheer-data-tui --version 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/sheer-data-tui --version")
   end
 end
